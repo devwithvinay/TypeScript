a17f34cf854bd965869182fe4ead7fe376992c07
vinay-here <krvinay785@gmail.com>
2026-08-25 16:46:43 +0530

    typscript-learning

---
+++ Typescript/typescript1/src/index.ts
@@ -0,0 +1,48 @@
+
+function Greet(name:string){
+    console.log("hello" , name)
+}
+
+Greet("Vinay")
+
+function Add(a:number , b:number){
+  
+  return a+b
+}
+
+let ans = Add(4,5)
+console.log(ans)
+
+function DelayedCall(fn:()=>void){
+    setTimeout(fn,1000)
+}
+
+DelayedCall(function(){
+    console.log('Hii Vinay');
+})
+
+//or
+
+function delay(anotherFn:()=>number){
+    setTimeout(anotherFn , 1000)
+}
+
+function log(){
+    return 1;
+}
+
+let value = delay(log)
+
+console.log(value)
+
+// or
+
+function Delaying(fn:(a:string)=>void){
+    setTimeout(fn,1000)
+}
+
+function greet(name:string){
+    console.log('hello'+name);
+}
+
+Delaying(greet)
