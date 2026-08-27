3c3bb753f583122e685b1bbdc0cf86b2a35803c0
vinay-here <krvinay785@gmail.com>
2026-08-27 10:22:01 +0530

    primitive types

--- Typescript/typescript2/src/index.ts
+++ Typescript/typescript2/src/index.ts
@@ -1,5 +1,5 @@
 
-
+// Number
 function sum(a:number , b:number): number{
     return a+b
 }
@@ -7,4 +7,24 @@ function sum(a:number , b:number): number{
 let ans = sum(2,4)
 console.log('sum is: '+ans);
 
+
+// String 
+
+function Greet(name:string):string{
+    return "hello"+name
+}
  
+Greet("Vinay")
+
+// Boolean 
+
+function isEven(num:number):boolean{
+    if(num%2==0){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+isEven(5)
